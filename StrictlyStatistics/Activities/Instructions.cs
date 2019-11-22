c4f986ae9dba1e8382ddf1075ca0dc63964e73d4
dylan <dylan.riley@nice.org.uk>
2019-11-22 16:13:37 +0000

    Setup db and load data into Instructions.axml

---
+++ StrictlyStatistics/Activities/Instructions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace StrictlyStatistics
+{
+    [Activity(Label = "Instructions")]
+    public class Instructions : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SQLiteRepository repo = new SQLiteRepository();
+
+            // Create your application here
+            SetContentView(Resource.Layout.Instructions);
+            var testView = FindViewById<TextView>(Resource.Id.textView2);
+            testView.Text = String.Join('\n', repo.GetAllDances().Select(x => x.Description).ToList());
+        }
+    }
+}
