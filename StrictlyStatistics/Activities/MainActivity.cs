00f454ac94d1e29a749f1f16a08b9b53a1494b4a
dylan <dylan.riley@nice.org.uk>
2019-12-06 17:41:24 +0000

    Reorganise SQLite code

--- StrictlyStatistics/Activities/MainActivity.cs
+++ StrictlyStatistics/Activities/MainActivity.cs
@@ -1,7 +1,6 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
-using Android.Runtime;
 using Android.Widget;
 using Android.Content;
 using StrictlyStatistics.Activities;
@@ -19,37 +18,7 @@ namespace StrictlyStatistics
             //https://hofmadresu.com/2018/04/29/todo-xamarin-native-android.html
             
             base.OnCreate(savedInstanceState);
-
-            Sqlite();
             CreateHandlers();
-        }
-
-        public void Sqlite()
-        {
-            var docFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
-            var dbFile = Path.Combine(docFolder, "StrictlyStats.db");
-            if (!System.IO.File.Exists(dbFile))
-            {
-                var stream = Resources.OpenRawResource(Resource.Raw.StrictlyStats);
-                FileStream writeStream = new FileStream(dbFile, FileMode.OpenOrCreate, FileAccess.Write);
-                ReadWriteStream(stream, writeStream);
-            }
-        }
-
-        private void ReadWriteStream(Stream readStream, Stream writeStream)
-        {
-            int length = 256;
-            byte[] buffer = new byte[length];
-            int bytesRead = readStream.Read(buffer, 0, length);
-
-            while(bytesRead > 0)
-            {
-                writeStream.Write(buffer, 0, bytesRead);
-                bytesRead = readStream.Read(buffer, 0, length);
-            }
-
-            readStream.Close();
-            writeStream.Close();
         }
 
         public void CreateHandlers()
--- StrictlyStatistics/App.cs
+++ StrictlyStatistics/App.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using Android.App;
 using Android.Runtime;
 using Autofac;
@@ -17,19 +18,40 @@ namespace StrictlyStatistics
         public override void OnCreate()
         {
             base.OnCreate();
+            Sqlite();
 
-            try
-            {
-                var builder = new ContainerBuilder();
-                builder.RegisterType<SQLiteRepository>().As<IRepository>().SingleInstance();
-                Container = builder.Build();
-            }
-            catch (Exception e)
-            {
-                var x = e.Message;
-            }
-            
+            var builder = new ContainerBuilder();
+            builder.RegisterType<SQLiteRepository>().As<IRepository>().SingleInstance();
+            Container = builder.Build();
 
+        }
+
+        public void Sqlite()
+        {
+            var docFolder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var dbFile = Path.Combine(docFolder, "StrictlyStats.db");
+            if (!System.IO.File.Exists(dbFile))
+            {
+                var stream = Resources.OpenRawResource(Resource.Raw.StrictlyStats);
+                FileStream writeStream = new FileStream(dbFile, FileMode.OpenOrCreate, FileAccess.Write);
+                ReadWriteStream(stream, writeStream);
+            }
+        }
+
+        private void ReadWriteStream(Stream readStream, Stream writeStream)
+        {
+            int length = 256;
+            byte[] buffer = new byte[length];
+            int bytesRead = readStream.Read(buffer, 0, length);
+
+            while (bytesRead > 0)
+            {
+                writeStream.Write(buffer, 0, bytesRead);
+                bytesRead = readStream.Read(buffer, 0, length);
+            }
+
+            readStream.Close();
+            writeStream.Close();
         }
     }
 }
