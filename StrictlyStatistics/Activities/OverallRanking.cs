ef64419e8d815d4c898d9df1625116eb0b83f1dd
dylan <dylan.riley@nice.org.uk>
2019-12-15 21:19:24 +0000

    Implement overall average ranking

--- StrictlyStatistics/Activities/OverallRanking.cs
+++ StrictlyStatistics/Activities/OverallRanking.cs
@@ -9,17 +9,31 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using StrictlyStatistics.UIComponents;
 
 namespace StrictlyStatistics.Activities
 {
     [Activity(Label = "OverallRanking")]
-    public class OverallRanking : Activity
+    public class OverallRanking : StrictlyStatsActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.OverallRanking);
 
-            // Create your application here
+            InitialiseOverallRankingListView();
+        }
+
+        void InitialiseOverallRankingListView()
+        {
+            var averageScores = new List<Tuple<string, int>>();
+            foreach (var c in Repo.GetAllCouples())
+            {
+                var averageScore = Repo.GetAllScores().Where(x => x.CoupleID == c.CoupleID).Average(x => x.ScoreValue);
+                averageScores.Add(new Tuple<string, int>(c.CoupleName, (int)averageScore));
+            }
+
+            RankingListView.Initialise(this, averageScores, Resource.Id.overallRankingListview, "The competition has not started yet.");
         }
     }
 }
