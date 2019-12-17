bd85c0772ab8555dd7a2d1dc9b02a9fb51bda03e
dylan <dylan.riley@nice.org.uk>
2019-12-17 22:59:47 +0000

    UPdate IRepository

--- StrictlyStatistics/Data/Repository.cs
+++ StrictlyStatistics/Data/Repository.cs
@@ -19,6 +19,13 @@ namespace StrictlyStatistics
         void SaveCoupleScore(Score score);
         void UpdateScore(Score score);
         void UpdateCouple(Couple couple);
+        void UpdateDance(Dance dance);
+        void CreateCouple(Dance couple);
+        void RemoveCouple(Dance couple);
+        void RemoveScore(Score score);
+        void CreateDance(Dance dance);
+        void RemoveDance(Dance dance);
+
 
     }
 
@@ -42,16 +49,22 @@ namespace StrictlyStatistics
         //Dances
         public List<Dance> GetAllDances() => con.Table<Dance>().ToList();
         public Dance GetDance(int id) => con.Table<Dance>().FirstOrDefault(x => x.DanceId == id);
+        public void UpdateDance(Dance dance) => con.Update(dance);
+        public void CreateDance(Dance dance) => con.Insert(dance);
+        public void RemoveDance(Dance dance) => con.Delete(dance);
 
         //Scores
         public List<Score> GetAllScores() => con.Table<Score>().ToList();
         public Score GetScore(int id) => con.Table<Score>().FirstOrDefault(x => x.ScoreID == id);
         public void SaveCoupleScore(Score score) => con.Insert(score);
         public void UpdateScore(Score score) => con.Update(score);
+        public void RemoveScore(Score score) => con.Delete(score);
 
         //Couples
         public List<Couple> GetAllCouples() => con.Table<Couple>().ToList();
         public Couple GetCouple(int id) => con.Table<Couple>().FirstOrDefault(x => x.CoupleID == id);
         public void UpdateCouple(Couple couple) => con.Update(couple);
+        public void CreateCouple(Dance couple) => con.Insert(couple);
+        public void RemoveCouple(Dance couple) => con.Delete(couple);
     }
 }
