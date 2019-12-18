d6033bac29d5ef643700dbbaf16f6ffb130bbeef
dylan <dylan.riley@nice.org.uk>
2019-12-18 23:29:49 +0000

    Implement edit score

--- StrictlyStatistics/Activities/EditScore.cs
+++ StrictlyStatistics/Activities/EditScore.cs
@@ -9,17 +9,110 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using StrictlyStatistics.Data.Models;
+using StrictlyStatistics.UIComponents;
 
 namespace StrictlyStatistics.Activities
 {
     [Activity(Label = "EditScore")]
-    public class EditScore : Activity
+    public class EditScore : StrictlyStatsActivity
     {
+        List<Score> CoupleScores { get; set; }
+        Score SelectedScore { get; set; }
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.EditScore);
 
-            // Create your application here
+            InitialiseButtons();
+            CoupleSpinner.Initialise(this, Repo.GetAllCouples(), Resource.Id.editScoreCoupleSpinner, true, PopulateScoresAndWeeks);
+            WeekSpinner.Initialise(this, weeks, Resource.Id.editScoreWeekSpinner, true, PopulateDances);
+            DanceSpinner.Initialise(this, Repo.GetAllDances(), Resource.Id.editScoreDanceSpinner, true, PopulateScore);
+            
         }
+
+        public void InitialiseButtons()
+        {
+
+            var scoreValue = FindViewById<EditText>(Resource.Id.editScoreValue);
+            scoreValue.TextChanged += (sender, args) =>
+            {
+                int score = 0;
+                int.TryParse(args.Text.ToString(), out score);
+                if (score != 0)
+                    SelectedScore.ScoreValue = score;
+            };
+
+            var saveButton = FindViewById<Button>(Resource.Id.saveButton);
+            saveButton.Click += (sender, args) =>
+            {
+                SelectedScore.CoupleID = Couple.CoupleID;
+                SelectedScore.DanceID = Dance.DanceId;
+                SelectedScore.WeekNumber = SelectedWeek;
+
+                if (SelectedScore.CoupleID == 0 ||
+                    SelectedScore.DanceID == 0 ||
+                    SelectedScore.ScoreID == 0 ||
+                    SelectedScore.ScoreValue == 0 ||
+                    SelectedScore.WeekNumber == 0)                    
+                    Alert.ShowAlertWithSingleButton(this, "Error", "All fields must be populated", "OK");
+                else if (SelectedScore.ScoreValue > 40)
+                    Alert.ShowAlertWithSingleButton(this, "Error", "Score value cannot be more than 40", "OK");
+                else if (SelectedScore.ScoreValue < 0)
+                    Alert.ShowAlertWithSingleButton(this, "Error", "Score value cannot be less than 0", "OK");
+                else if (SelectedScore.ScoreID != 0)
+                {
+                    Repo.UpdateScore(SelectedScore);
+                    Alert.ShowAlertWithSingleButton(this, "Success", "Score saved!", "OK");
+                }
+            };
+
+            var deleteButton = FindViewById<Button>(Resource.Id.deleteButton);
+            deleteButton.Click += (sender, args) =>
+            {
+                if (SelectedScore.ScoreID != 0)
+                {
+                    Repo.RemoveScore(SelectedScore);
+                    Alert.ShowAlertWithSingleButton(this, "Success", "Score deleted", "OK");
+                }
+            };
+
+            var cancelButton = FindViewById<Button>(Resource.Id.cancelButton);
+            cancelButton.Click += (sender, args) => StartActivity(new Intent(this, typeof(EditScore)));
+        }
+
+        public void PopulateScoresAndWeeks()
+        {
+            CoupleScores = Repo.GetAllScores().Where(x => x.CoupleID == Couple.CoupleID).ToList();
+            var possibleWeeks = weeks.Where(x => CoupleScores.Select(z => z.WeekNumber).Contains(x)).ToList();
+            WeekSpinner.Update(this, possibleWeeks, Resource.Id.editScoreWeekSpinner);
+        }
+
+        public void PopulateDances()
+        {
+            var possibleDanceIds = CoupleScores.Where(x => 
+                                    x.CoupleID == Couple.CoupleID && 
+                                    x.WeekNumber == SelectedWeek)
+                                   .Select(x => x.DanceID)
+                                   .ToList();
+
+            var possibleDances = Repo.GetAllDances().Where(x => 
+                                 possibleDanceIds.Contains(x.DanceId))
+                                 .ToList();
+
+            DanceSpinner.Update(this, possibleDances, Resource.Id.editScoreDanceSpinner);
+        }
+
+        public void PopulateScore()
+        {
+            SelectedScore = CoupleScores.Where(x => x.WeekNumber == SelectedWeek &&
+                                               x.CoupleID == Couple.CoupleID)
+                                               .FirstOrDefault() ?? new Score();
+
+            var scoreValue = FindViewById<EditText>(Resource.Id.editScoreValue);
+            scoreValue.Text = SelectedScore.ScoreValue.ToString();
+        }
+            
+        
     }
 }
--- StrictlyStatistics/Resources/Resource.designer.cs
+++ StrictlyStatistics/Resources/Resource.designer.cs
@@ -2360,14 +2360,14 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f090090
 			public const int VoteButton = 2131296400;
 			
-			// aapt resource value: 0x7f0900e5
-			public const int WeekStatsTitle = 2131296485;
+			// aapt resource value: 0x7f0900eb
+			public const int WeekStatsTitle = 2131296491;
 			
 			// aapt resource value: 0x7f09008e
 			public const int WeeklyRankingsButton = 2131296398;
 			
-			// aapt resource value: 0x7f0900c4
-			public const int action0 = 2131296452;
+			// aapt resource value: 0x7f0900cb
+			public const int action0 = 2131296459;
 			
 			// aapt resource value: 0x7f09007c
 			public const int action_bar = 2131296380;
@@ -2390,17 +2390,17 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f09005a
 			public const int action_bar_title = 2131296346;
 			
-			// aapt resource value: 0x7f0900c1
-			public const int action_container = 2131296449;
+			// aapt resource value: 0x7f0900c8
+			public const int action_container = 2131296456;
 			
 			// aapt resource value: 0x7f09007d
 			public const int action_context_bar = 2131296381;
 			
-			// aapt resource value: 0x7f0900c8
-			public const int action_divider = 2131296456;
+			// aapt resource value: 0x7f0900cf
+			public const int action_divider = 2131296463;
 			
-			// aapt resource value: 0x7f0900c2
-			public const int action_image = 2131296450;
+			// aapt resource value: 0x7f0900c9
+			public const int action_image = 2131296457;
 			
 			// aapt resource value: 0x7f090003
 			public const int action_menu_divider = 2131296259;
@@ -2417,11 +2417,11 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f09005c
 			public const int action_mode_close_button = 2131296348;
 			
-			// aapt resource value: 0x7f0900c3
-			public const int action_text = 2131296451;
+			// aapt resource value: 0x7f0900ca
+			public const int action_text = 2131296458;
 			
-			// aapt resource value: 0x7f0900d1
-			public const int actions = 2131296465;
+			// aapt resource value: 0x7f0900d8
+			public const int actions = 2131296472;
 			
 			// aapt resource value: 0x7f09005d
 			public const int activity_chooser_view_content = 2131296349;
@@ -2456,11 +2456,11 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f090063
 			public const int buttonPanel = 2131296355;
 			
-			// aapt resource value: 0x7f0900dc
-			public const int cancelButton = 2131296476;
+			// aapt resource value: 0x7f0900c6
+			public const int cancelButton = 2131296454;
 			
-			// aapt resource value: 0x7f0900c5
-			public const int cancel_action = 2131296453;
+			// aapt resource value: 0x7f0900cc
+			public const int cancel_action = 2131296460;
 			
 			// aapt resource value: 0x7f0900ab
 			public const int celebFirstNameInput = 2131296427;
@@ -2483,8 +2483,8 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f090073
 			public const int checkbox = 2131296371;
 			
-			// aapt resource value: 0x7f0900cd
-			public const int chronometer = 2131296461;
+			// aapt resource value: 0x7f0900d4
+			public const int chronometer = 2131296468;
 			
 			// aapt resource value: 0x7f09004e
 			public const int clip_horizontal = 2131296334;
@@ -2495,8 +2495,8 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f090039
 			public const int collapseActionView = 2131296313;
 			
-			// aapt resource value: 0x7f0900db
-			public const int confirmButton = 2131296475;
+			// aapt resource value: 0x7f0900e2
+			public const int confirmButton = 2131296482;
 			
 			// aapt resource value: 0x7f09009c
 			public const int container = 2131296412;
@@ -2507,8 +2507,8 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f09009d
 			public const int coordinator = 2131296413;
 			
-			// aapt resource value: 0x7f0900d8
-			public const int coupleInput = 2131296472;
+			// aapt resource value: 0x7f0900df
+			public const int coupleInput = 2131296479;
 			
 			// aapt resource value: 0x7f090097
 			public const int couplesBreakdownTitle = 2131296407;
@@ -2522,8 +2522,8 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f09006c
 			public const int customPanel = 2131296364;
 			
-			// aapt resource value: 0x7f0900d9
-			public const int danceInput = 2131296473;
+			// aapt resource value: 0x7f0900e0
+			public const int danceInput = 2131296480;
 			
 			// aapt resource value: 0x7f090099
 			public const int dancesListView = 2131296409;
@@ -2533,6 +2533,9 @@ namespace StrictlyStatistics
 			
 			// aapt resource value: 0x7f090060
 			public const int default_activity_button = 2131296352;
+			
+			// aapt resource value: 0x7f0900c5
+			public const int deleteButton = 2131296453;
 			
 			// aapt resource value: 0x7f0900bf
 			public const int deleteDanceButton = 2131296447;
@@ -2594,14 +2597,26 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f090096
 			public const int editScoreButton = 2131296406;
 			
+			// aapt resource value: 0x7f0900c0
+			public const int editScoreCoupleSpinner = 2131296448;
+			
+			// aapt resource value: 0x7f0900c2
+			public const int editScoreDanceSpinner = 2131296450;
+			
+			// aapt resource value: 0x7f0900c3
+			public const int editScoreValue = 2131296451;
+			
+			// aapt resource value: 0x7f0900c1
+			public const int editScoreWeekSpinner = 2131296449;
+			
 			// aapt resource value: 0x7f09007e
 			public const int edit_query = 2131296382;
 			
 			// aapt resource value: 0x7f090030
 			public const int end = 2131296304;
 			
-			// aapt resource value: 0x7f0900d3
-			public const int end_padder = 2131296467;
+			// aapt resource value: 0x7f0900da
+			public const int end_padder = 2131296474;
 			
 			// aapt resource value: 0x7f09003f
 			public const int enterAlways = 2131296319;
@@ -2645,8 +2660,8 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f090062
 			public const int icon = 2131296354;
 			
-			// aapt resource value: 0x7f0900d2
-			public const int icon_group = 2131296466;
+			// aapt resource value: 0x7f0900d9
+			public const int icon_group = 2131296473;
 			
 			// aapt resource value: 0x7f09003a
 			public const int ifRoom = 2131296314;
@@ -2654,11 +2669,11 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f09005f
 			public const int image = 2131296351;
 			
-			// aapt resource value: 0x7f0900ce
-			public const int info = 2131296462;
+			// aapt resource value: 0x7f0900d5
+			public const int info = 2131296469;
 			
-			// aapt resource value: 0x7f0900c0
-			public const int instructionsText = 2131296448;
+			// aapt resource value: 0x7f0900c7
+			public const int instructionsText = 2131296455;
 			
 			// aapt resource value: 0x7f090059
 			public const int italic = 2131296345;
@@ -2684,14 +2699,14 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f090061
 			public const int list_item = 2131296353;
 			
-			// aapt resource value: 0x7f0900e8
-			public const int masked = 2131296488;
+			// aapt resource value: 0x7f0900ee
+			public const int masked = 2131296494;
 			
-			// aapt resource value: 0x7f0900c7
-			public const int media_actions = 2131296455;
+			// aapt resource value: 0x7f0900ce
+			public const int media_actions = 2131296462;
 			
-			// aapt resource value: 0x7f0900de
-			public const int message = 2131296478;
+			// aapt resource value: 0x7f0900e4
+			public const int message = 2131296484;
 			
 			// aapt resource value: 0x7f090031
 			public const int middle = 2131296305;
@@ -2714,20 +2729,20 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f09001e
 			public const int normal = 2131296286;
 			
+			// aapt resource value: 0x7f0900d7
+			public const int notification_background = 2131296471;
+			
+			// aapt resource value: 0x7f0900d1
+			public const int notification_main_column = 2131296465;
+			
 			// aapt resource value: 0x7f0900d0
-			public const int notification_background = 2131296464;
-			
-			// aapt resource value: 0x7f0900ca
-			public const int notification_main_column = 2131296458;
-			
-			// aapt resource value: 0x7f0900c9
-			public const int notification_main_column_container = 2131296457;
+			public const int notification_main_column_container = 2131296464;
 			
 			// aapt resource value: 0x7f090092
 			public const int overallRankingButton = 2131296402;
 			
-			// aapt resource value: 0x7f0900d4
-			public const int overallRankingListview = 2131296468;
+			// aapt resource value: 0x7f0900db
+			public const int overallRankingListview = 2131296475;
 			
 			// aapt resource value: 0x7f09004c
 			public const int parallax = 2131296332;
@@ -2759,20 +2774,23 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f090091
 			public const int rankingByDanceButton = 2131296401;
 			
-			// aapt resource value: 0x7f0900d6
-			public const int rankingByDanceListView = 2131296470;
+			// aapt resource value: 0x7f0900dd
+			public const int rankingByDanceListView = 2131296477;
 			
-			// aapt resource value: 0x7f0900d5
-			public const int rankingByDanceSpinner = 2131296469;
+			// aapt resource value: 0x7f0900dc
+			public const int rankingByDanceSpinner = 2131296476;
 			
 			// aapt resource value: 0x7f09004a
 			public const int right = 2131296330;
 			
-			// aapt resource value: 0x7f0900cf
-			public const int right_icon = 2131296463;
+			// aapt resource value: 0x7f0900d6
+			public const int right_icon = 2131296470;
 			
-			// aapt resource value: 0x7f0900cb
-			public const int right_side = 2131296459;
+			// aapt resource value: 0x7f0900d2
+			public const int right_side = 2131296466;
+			
+			// aapt resource value: 0x7f0900c4
+			public const int saveButton = 2131296452;
 			
 			// aapt resource value: 0x7f09000c
 			public const int save_image_matrix = 2131296268;
@@ -2783,11 +2801,11 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f09000e
 			public const int save_scale_type = 2131296270;
 			
-			// aapt resource value: 0x7f0900da
-			public const int scoreInput = 2131296474;
+			// aapt resource value: 0x7f0900e1
+			public const int scoreInput = 2131296481;
 			
-			// aapt resource value: 0x7f0900e4
-			public const int scoresList = 2131296484;
+			// aapt resource value: 0x7f0900ea
+			public const int scoresList = 2131296490;
 			
 			// aapt resource value: 0x7f090029
 			public const int screen = 2131296297;
@@ -2882,8 +2900,8 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f09004b
 			public const int start = 2131296331;
 			
-			// aapt resource value: 0x7f0900c6
-			public const int status_bar_latest_event_content = 2131296454;
+			// aapt resource value: 0x7f0900cd
+			public const int status_bar_latest_event_content = 2131296461;
 			
 			// aapt resource value: 0x7f090076
 			public const int submenuarrow = 2131296374;
@@ -2939,8 +2957,8 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f090015
 			public const int textinput_error = 2131296277;
 			
-			// aapt resource value: 0x7f0900cc
-			public const int time = 2131296460;
+			// aapt resource value: 0x7f0900d3
+			public const int time = 2131296467;
 			
 			// aapt resource value: 0x7f09001c
 			public const int title = 2131296284;
@@ -2987,35 +3005,35 @@ namespace StrictlyStatistics
 			// aapt resource value: 0x7f090016
 			public const int view_offset_helper = 2131296278;
 			
+			// aapt resource value: 0x7f0900ed
+			public const int visible = 2131296493;
+			
+			// aapt resource value: 0x7f0900e6
+			public const int voteCoupleSpinner = 2131296486;
+			
+			// aapt resource value: 0x7f0900e8
+			public const int voteOffButton = 2131296488;
+			
+			// aapt resource value: 0x7f0900e5
+			public const int voteOffTitle = 2131296485;
+			
 			// aapt resource value: 0x7f0900e7
-			public const int visible = 2131296487;
-			
-			// aapt resource value: 0x7f0900e0
-			public const int voteCoupleSpinner = 2131296480;
-			
-			// aapt resource value: 0x7f0900e2
-			public const int voteOffButton = 2131296482;
-			
-			// aapt resource value: 0x7f0900df
-			public const int voteOffTitle = 2131296479;
-			
-			// aapt resource value: 0x7f0900e1
-			public const int voteOffWeekSpinner = 2131296481;
+			public const int voteOffWeekSpinner = 2131296487;
 			
 			// aapt resource value: 0x7f0900b5
 			public const int votedOffWeekNumber = 2131296437;
 			
-			// aapt resource value: 0x7f0900d7
-			public const int weekInput = 2131296471;
+			// aapt resource value: 0x7f0900de
+			public const int weekInput = 2131296478;
+			
+			// aapt resource value: 0x7f0900e9
+			public const int weekRankingInput = 2131296489;
+			
+			// aapt resource value: 0x7f0900ec
+			public const int weekStatsText = 2131296492;
 			
 			// aapt resource value: 0x7f0900e3
-			public const int weekRankingInput = 2131296483;
-			
-			// aapt resource value: 0x7f0900e6
-			public const int weekStatsText = 2131296486;
-			
-			// aapt resource value: 0x7f0900dd
-			public const int weeksStatsButton = 2131296477;
+			public const int weeksStatsButton = 2131296483;
 			
 			// aapt resource value: 0x7f09003c
 			public const int withText = 2131296316;
